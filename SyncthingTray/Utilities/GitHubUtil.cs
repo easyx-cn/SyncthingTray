8f1c4af089455d2e33c2a06ca8c654a1534ed4fb
xiongfj <837943056@qq.com>
2025-08-25 17:10:59 +0800

    移除 Octokit 组件，该组件限制下载频率，改为使用 HttpClient 显示进度的方式下载

--- SyncthingTray/Utilities/GitHubUtil.cs
+++ SyncthingTray/Utilities/GitHubUtil.cs
@@ -1,97 +1,108 @@
-﻿using Octokit;
-using SyncthingTray.Properties;
+﻿using SyncthingTray.Properties;
 using SyncthingTray.UI.Dialogs;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Windows.Forms;
 
 namespace SyncthingTray.Utilities
 {
-    public static class GitHubUtil
-    {
-        private static readonly GitHubClient GithubClient = new GitHubClient(new ProductHeaderValue(Settings.Default.ApplicationName));
+	public static class GitHubUtil
+	{
+		public static void GetLatestVersion()
+		{
+			var paDialog = new PendingActivityDialog
+			{
+				Text = "Autoupdate syncthing",
+				SupportsCancellation = false,
+				SupportsProgressVisualization = false
+			};
+			paDialog.BackgroundWorker.DoWork += delegate
+			{
+				paDialog.BackgroundWorker.ReportProgress(0, "Checking for latest version...");
 
-        public static void GetLatestVersion()
-        {
-            var paDialog = new PendingActivityDialog
-            {
-                Text = "Autoupdate syncthing",
-                SupportsCancellation = false,
-                SupportsProgressVisualization = false
-            };
-            paDialog.BackgroundWorker.DoWork += delegate
-            {
-                paDialog.BackgroundWorker.ReportProgress(0, "Checking for latest version...");
+				string url = Environment.Is64BitOperatingSystem
+					? "https://github.com/syncthing/syncthing/releases/download/v2.0.3/syncthing-windows-amd64-v2.0.3.zip"
+					: "https://github.com/syncthing/syncthing/releases/download/v2.0.3/syncthing-windows-386-v2.0.3.zip";
 
-                var repos = GithubClient.Release.GetAll("syncthing", "syncthing").Result;
-                var repoIndex = 0;
-                ReleaseAsset asset = null;
+				using (var client = new HttpClient())
+				{
+					// 获取 syncthing 下载地址，却分 64 位 32 位
+					var links = client.GetStringAsync("https://github.com/easyx-cn/SyncthingTray/releases/download/syncthing/syncthing_last_version.json").Result;
+					Regex rex;
+					if(Environment.Is64BitOperatingSystem)
+						rex = new Regex($@"\[AMD64\](.+)\r|\n");
+					else
+						rex = new Regex($@"\[386\](.+)\r|\n");
+					var ms = rex.Match(links);
+					if (!ms.Success)
+					{
+						paDialog.BackgroundWorker.ReportProgress(0, "获取下载地址失败！");
+						Thread.Sleep(5000);
+						return;
+					}
+					string syncthing_download_address = ms.Groups[1].Value;
 
-                while (asset == null && repoIndex < repos.Count)
-                {
-                    var latest = GithubClient.Release.GetAssets("syncthing", "syncthing", repos[0].Id).Result;
-                    var versions = GrabVersions(latest, repos[repoIndex++]);
-                    asset = Environment.Is64BitOperatingSystem ? versions.LatestAmd64 : versions.LatestIntel386;
-                }
+					using (var r = client.GetAsync(syncthing_download_address, HttpCompletionOption.ResponseHeadersRead).Result)
+					{
+						r.EnsureSuccessStatusCode();
 
-                if (asset == null) return;
-                paDialog.BackgroundWorker.ReportProgress(0, $"Downloading {asset.Name}...");
-                var response = GithubClient.Connection.Get<byte[]>(new Uri(asset.Url), new Dictionary<string, string>(), "application/octet-stream").Result;
+						using (Stream re = r.Content.ReadAsStreamAsync().Result)
+						{
+							var filename = Path.GetTempFileName();
+							using (FileStream sfile = File.Create(filename))
+							{
+								byte[] buffer = new byte[1024];
+								int bytesRead;
+								float readLength = 0;
+								long totalLength = r.Content.Headers.ContentLength ?? -1;
 
-                paDialog.BackgroundWorker.ReportProgress(50, $"Unzipping {asset.Name}...");
-                var filename = Path.GetTempFileName();
-                using (var fileStream = File.Create(filename))
-                {
-                    fileStream.Write(response.BodyAsObject, 0, response.BodyAsObject.Length);
-                }
-                System.IO.Compression.ZipFile.ExtractToDirectory(filename, Path.GetDirectoryName(System.Windows.Forms.Application.ExecutablePath));
-                Directory.Move(Path.Combine(Path.GetDirectoryName(System.Windows.Forms.Application.ExecutablePath), Path.GetFileNameWithoutExtension(asset.Name)), Path.Combine(Path.GetDirectoryName(System.Windows.Forms.Application.ExecutablePath), "syncthing"));
-                if (string.IsNullOrEmpty(Settings.Default.SyncthingPath))
-                {
-                    Settings.Default.SyncthingPath = Path.Combine(Path.Combine(Path.GetDirectoryName(System.Windows.Forms.Application.ExecutablePath), "syncthing"), "syncthing.exe");
-                    Settings.Default.Save();
-                }
+								float currentProgress = 0.0f;
+								while ((bytesRead = re.Read(buffer, 0, buffer.Length)) > 0)
+								{
+									readLength += bytesRead;
 
-                File.Delete(filename);
-                paDialog.BackgroundWorker.ReportProgress(100, "Finished!");
-                Thread.Sleep(500);
-            };
-            paDialog.BackgroundWorker.RunWorkerCompleted += delegate (object sender, RunWorkerCompletedEventArgs args)
-            {
-                if (args.Error != null)
-                {
-                    MessageBox.Show(args.Error.Message);
-                }
-            };
-            paDialog.ShowDialog();
-        }
+									float p = (float)Math.Round((decimal)(readLength / totalLength) * 100, 2);
+									if (p > currentProgress + 0.2)
+									{
+										currentProgress = p;
+										paDialog.BackgroundWorker.ReportProgress(50, $"Downloading ({p}%)");
+									}
+									sfile.Write(buffer, 0, bytesRead);
+								}
+							}
+							string zipName = url.Substring(url.LastIndexOf('/') + 1);
+							paDialog.BackgroundWorker.ReportProgress(99, $"Unzipping {zipName}...");
 
-        private static LatestVersionRetrievedEventArgs GrabVersions(IEnumerable<ReleaseAsset> latest, Release release)
-        {
-            var windowsLatest = latest.Where(asset => asset.Name.Contains("windows")).ToList();
-            var windowsIntel386 = windowsLatest.FirstOrDefault(asset => asset.Name.Contains("386"));
-            var windowsAmd64 = windowsLatest.FirstOrDefault(asset => asset.Name.Contains("amd64"));
+							System.IO.Compression.ZipFile.ExtractToDirectory(filename, Path.GetDirectoryName(System.Windows.Forms.Application.ExecutablePath));
+							Directory.Move(Path.Combine(Path.GetDirectoryName(System.Windows.Forms.Application.ExecutablePath), Path.GetFileNameWithoutExtension(zipName)), Path.Combine(Path.GetDirectoryName(System.Windows.Forms.Application.ExecutablePath), "syncthing"));
+							if (string.IsNullOrEmpty(Settings.Default.SyncthingPath))
+							{
+								Settings.Default.SyncthingPath = Path.Combine(Path.Combine(Path.GetDirectoryName(System.Windows.Forms.Application.ExecutablePath), "syncthing"), "syncthing.exe");
+								Settings.Default.Save();
+							}
 
-            return new LatestVersionRetrievedEventArgs()
-            {
-                LatestIntel386 = windowsIntel386,
-                LatestAmd64 = windowsAmd64,
-                LatestVersion = release
-            };
-        }
-
-    }
-
-    public class LatestVersionRetrievedEventArgs
-    {
-        public ReleaseAsset LatestIntel386 { get; set; }
-
-        public ReleaseAsset LatestAmd64 { get; set; }
-
-        public Release LatestVersion { get; set; }
-    }
+							File.Delete(filename);
+							paDialog.BackgroundWorker.ReportProgress(100, "Finished!");
+							Thread.Sleep(500);
+						}
+					}
+				}
+			};
+			paDialog.BackgroundWorker.RunWorkerCompleted += delegate (object sender, RunWorkerCompletedEventArgs args)
+			{
+				if (args.Error != null)
+				{
+					MessageBox.Show(args.Error.Message);
+				}
+			};
+			paDialog.ShowDialog();
+		}
+	}
 }
